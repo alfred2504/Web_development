c0830fca3263bc3a723504c179b399af5da0ec64
alfred2504 <inforamaiv@gmail.com>
2024-04-26 10:12:27 +0200

    update web dev

---
+++ js_basics/index.js
@@ -0,0 +1,21 @@
+// This is my first JavaScript code!
+console.log('Hello World');
+
+let name = 'Alfred'; 
+console.log(name);
+
+// Cannot be a reserve keyword
+// Should be meaningful
+// They cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+
+let firstName = 'Alfred'; // String Literal
+let age = '30';
+let isApproved = true; // Boolean Literal
+let fisrtName = undefined;
+let lastName = null;
+let selectedColor ='null';
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log('interestRate');
